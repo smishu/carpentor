c60c7de1ce6388c7ebb272a129658ce76314600d
Abraham851 <smishu851@gmail.com>
2022-05-23 14:41:42 +0600

    banner add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './Pages/Home/Home';
+
+
 
 function App() {
   return (
     <div>
       <h1 className='text-secondary'>Cerpenco</h1>
 
-    </div>
+      <Routes>
+        <Route path='' element="<Home/>"></Route>
+
+      </Routes>
+
+    </div >
   );
 }
 
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import silder1 from '../../Assets/carpenter-banner.webp';
+import silder2 from '../../Assets/slider-2.webp';
+import silder3 from '../../Assets/slider-3.webp';
+
+const Banner = () => {
+    return (
+
+        <div class="carousel w-full">
+            <div id="slide1" class="carousel-item relative w-full">
+                <img src={silder1} alt='' class="w-full" /> /
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" class="btn btn-circle">❮</a>
+                    <a href="#slide2" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" class="carousel-item relative w-full">
+                <img src={silder2} alt='' class="w-full" /> /
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" class="btn btn-circle">❮</a>
+                    <a href="#slide3" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" class="carousel-item relative w-full">
+                <img src={silder3} alt='' class="w-full" /> /
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" class="btn btn-circle">❮</a>
+                    <a href="#slide4" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Banner;
